340e06d3b373e6eeb7b3b763471949a0ae1a609b
Brian Henk <brianhenk@gmail.com>
2023-01-26 05:49:13 -0800

    Another Transition Bug
    Fixed error Unexpected exception: This transition (askAim) is impossible at this state (44)

--- goodcopbadcop/states.inc.php
+++ goodcopbadcop/states.inc.php
@@ -369,7 +369,7 @@ $machinestates = array(
     		"type" => "activeplayer",
         'args' => 'argGetPlayerButtonTargets',
     		"possibleactions" => array( "clickPlayer" ),
-    		"transitions" => array( "executeEquipment" => 31, "choosePlayer" => 41, "chooseActiveOrHandEquipmentCard" => 42, "playerTurn" => 60, "chooseEquipmentToPlayReactEndOfTurn" => 16, "chooseEquipmentToPlayReactInvestigate" => 17, "chooseEquipmentToPlayReactShoot" => 18, "chooseEquipmentToPlayOnYourTurn" => 15, "chooseEquipmentToPlayReactBite" => 19, "discardEquipment" => 12, "determineActivePlayerAfterTurnAction"=> 63, "chooseEquipmentToPlayStartGame" => 61, "endTurnReaction" => 29, "chooseEquipmentTargetOutOfTurn" => 32 )
+    		"transitions" => array( "executeEquipment" => 31, "choosePlayer" => 41, "chooseActiveOrHandEquipmentCard" => 42, "playerTurn" => 60, "chooseEquipmentToPlayReactEndOfTurn" => 16, "chooseEquipmentToPlayReactInvestigate" => 17, "chooseEquipmentToPlayReactShoot" => 18, "chooseEquipmentToPlayOnYourTurn" => 15, "chooseEquipmentToPlayReactBite" => 19, "discardEquipment" => 12, "determineActivePlayerAfterTurnAction"=> 63, "chooseEquipmentToPlayStartGame" => 61, "endTurnReaction" => 29, "chooseEquipmentTargetOutOfTurn" => 32, "askAim" => 27 )
     ),
 
     45 => array(
